c08dd3a8e4fb4d7d94ee6c87350212fd81f35efe
Lauro C Oliveira <lauro@hostcert.com.br>
2023-11-29 22:40:05 -0300

    Adicionado Aptos no Admin

--- apartamentos/app_catalogo/admin.py
+++ apartamentos/app_catalogo/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Apartamentos
+
+class ApartamentosAdmin(admin.ModelAdmin):
+    """
+    Classe para descrever o form de edicao,criacao e detail do model
+    """
+    save_on_top = True 
+    list_display = ["morador","numero_apto"]
+    search_fields =["morador","numero_apto"]
+    list_editable = ["numero_apto"]
+
+
+admin.site.register(Apartamentos,ApartamentosAdmin)
