2ee5bccc0a35a1d6b4173d0b90ec24c2d0ec5c08
AlexOliveiraK <alex.mcaste@gmail.com>
2023-12-04 08:42:34 -0300

    "corrigido menu e exibição do conteudo"

--- apartamentos/apartamentos/settings.py
+++ apartamentos/apartamentos/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_producao')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/app_catalogo/')
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 
